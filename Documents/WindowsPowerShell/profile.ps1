2b3acf2b989f688c43b2422e2c4cd5933e879e24
Paweł Białaszczyk <pawelbialaszczyk@gmail.com>
2018-03-29 20:36:06 +0200

    Add dotnet command tab completion

--- Documents/WindowsPowerShell/profile.ps1
+++ Documents/WindowsPowerShell/profile.ps1
@@ -7,3 +7,10 @@ $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
 function dotfiles {
     git --git-dir=$HOME/.dotfiles --work-tree=$HOME $args
 }
+
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+    dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
